471a4d118b7ec3fb24d535c9d62c8b5eb5c68a64
Mansikulkarni2603 <69478326+Mansikulkarni2603@users.noreply.github.com>
2025-09-16 18:18:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 var numberOfDrums = document.querySelectorAll(".drum").length;
+
 for(var i=0; i<numberOfDrums; i++){
     document.querySelectorAll(".drum")[i].addEventListener("click", function(){
-        var drumPlayed = this.innerHTML;
+        var drumInnerHTML = this.innerHTML;
 
-        switch (drumPlayed) {
+        switch (drumInnerHTML) {
             case "w":
-                var tom1 = new Audio("./sounds/tom-1.mp3");
+                var tom1 = new Audio("../sounds/tom-1.mp3");
                 tom1.play();
                 break;
             case "a":
-                var tom2 = new Audio("./sounds/tom-2.mp3");
+                var tom2 = new Audio("../sounds/tom-2.mp3");
                 tom2.play();
                 break;
             case "s":
